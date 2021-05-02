dcdba90600e09cbcb9f283dde219d698d01f9802
woogers <woogers@gmail.com>
2021-05-02 11:24:22 -0400

    cleaning

--- go.py
+++ go.py
@@ -13,7 +13,8 @@ import matplotlib.font_manager as mfm
 
 
 def char_in_font(uc_char, font):
-    for cmap in font['cmap'].tables:
+    """See if a given unicode character is in a given font"""
+    for cmap in font["cmap"].tables:
         if cmap.isUnicode():
             if ord(uc_char) in cmap.cmap:
                 return True
@@ -22,12 +23,12 @@ def char_in_font(uc_char, font):
 
 # Config parameters
 cp = configparser.RawConfigParser()
-cp_path = 'config.ini'
+cp_path = "config.ini"
 cp.read(cp_path)
 
-work_dir = cp.get('config', 'install_path')
-api_url = cp.get('config', 'api_url')
-contest_id = cp.get('config', 'contest_id')
+work_dir = cp.get("config", "install_path")
+api_url = cp.get("config", "api_url")
+contest_id = cp.get("config", "contest_id")
 
 # Get league matches to find closest
 url = f"{api_url}/contests/{contest_id}/sessions"
@@ -52,7 +53,10 @@ for side in closest["plannedMatches"]:
 
 # Copy team logos for selected teams from Source Images to Schedule Images
 for i in range(0, len(selected_teams)):
-    shutil.copy(f"{work_dir}MJSL OBS TOOLS/Source Images/{selected_teams[i]['_id']}.png", f"{work_dir}/MJSL OBS TOOLS/Script Images/Schedule/Schedule-{i+1:02d}.png")
+    shutil.copy(
+        f"{work_dir}MJSL OBS TOOLS/Source Images/{selected_teams[i]['_id']}.png",
+        f"{work_dir}/MJSL OBS TOOLS/Script Images/Schedule/Schedule-{i+1:02d}.png",
+    )
 
 # Generate Script Text files
 utc = pytz.utc
@@ -62,7 +66,7 @@ with open(f"{work_dir}/MJSL OBS TOOLS/Script Text/General/Time.txt", "w") as f:
     f.write(time_formatted)
 
 with open(f"{work_dir}/MJSL OBS TOOLS/Script Text/General/Session.txt", "w") as f:
-    session_name = closest['name']
+    session_name = closest["name"]
     f.write(session_name)
 
 print(
@@ -97,16 +101,16 @@ matches_passed = 0
 
 for session in sessions:
     if session["scheduledTime"] < now:
-        matches_axis.append(session['name'])
+        matches_axis.append(session["name"])
         matches_passed += 1
-
-# matches_axis.extend(range(0, matches_passed + 1))
 
 font_info = [(f.fname, f.name) for f in mfm.fontManager.ttflist]
 font_path = None
 
 # Sort teams score table from closest match
-sorted_teams = sorted(closest['aggregateTotals'].items(), key=lambda x: x[1], reverse=True)
+sorted_teams = sorted(
+    closest["aggregateTotals"].items(), key=lambda x: x[1], reverse=True
+)
 
 for steam in sorted_teams:
     scores = [0]
@@ -114,18 +118,24 @@ for steam in sorted_teams:
         scores.append(sessions[i]["aggregateTotals"][steam[0]] / 1000)
 
     # Get team info from contest
-    contest_team = next((cteam for cteam in contest['teams'] if cteam['_id'] == steam[0]))
+    contest_team = next(
+        (cteam for cteam in contest["teams"] if cteam["_id"] == steam[0])
+    )
 
     # Check for unicode team names because why god why
-    for x in contest_team['name']:
+    for x in contest_team["name"]:
         if ord(x) > 127:
             while not font_path:
                 for i, font in enumerate(font_info):
+                    # Find a font on user's machine that supports the team name
                     if char_in_font(x, TTFont(font[0], fontNumber=0)):
                         font_path = font[0]
 
     plt.plot(
-        matches_axis, scores, label=f"{contest_team['name']}: {scores[len(scores) - 1]:.1f}", color=f"#{contest_team['color']}"
+        matches_axis,
+        scores,
+        label=f"{contest_team['name']}: {scores[len(scores) - 1]:.1f}",
+        color=f"#{contest_team['color']}",
     )
 
 prop = mfm.FontProperties(fname=font_path)
